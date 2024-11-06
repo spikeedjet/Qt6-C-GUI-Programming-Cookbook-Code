65f96e2444e38d801fbbdbb48cb6199f78709a90
spikeedjet <spike.edjet@gmail.com>
2024-11-06 14:46:49 +0800

    add opengl asySlotSignal and fuctioncallback

---
+++ AsynchronousSignalSlot/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <QCoreApplication>
+#include <QNetworkReply>
+#include <QDebug>
+#include<QNetworkAccessManager>
+
+void printHTML (QString html){
+    qDebug()<<"Done";
+    qDebug()<<html;
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QString *html = new QString;
+    qDebug() << "Start";
+    QNetworkAccessManager manager;
+    QNetworkRequest req(QUrl ("http://www.dustyfeet.com"));
+    QNetworkReply* reply = manager.get(req);
+
+    QObject::connect(reply,&QNetworkReply::readyRead,[=](){
+        html->append(QString(reply->readAll()));
+    });
+    QObject::connect(reply,&QNetworkReply::downloadProgress,[](qint64 bytesReceived, qint64 bytesTotal){
+        qDebug()<< "Progress: " << bytesReceived << "bytes /"
+                 << bytesTotal << "bytes";
+    });
+    QObject::connect(reply,&QNetworkReply::finished,[=](){
+        printHTML(*html);
+    });
+
+    return a.exec();
+}
---
+++ OpenGLImplementation/main.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <QtOpenGL>
+
+#include <QApplication>
+#include "renderwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    RenderWindow window;
+    window.setTitle("hello world");
+    window.resize(640,480);
+    window.show();
+    return a.exec();
+}
